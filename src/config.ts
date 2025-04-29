5563dd99bdde05ff8ff385728c1ccf4342a42318
youngyu348 <60997382+youngyu348@users.noreply.github.com>
2025-04-29 21:50:03 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const themeConfig: ThemeConfig = {
     url: 'https://retypeset.radishzz.cc',
     // favicon url
     // recommended formats: svg, png or ico
-    favicon: '/icon/favicon3.png', // or https://example.com/favicon.svg
+    favicon: '/icon/favicon.png', // or https://example.com/favicon.svg
   },
   // SITE INFORMATION >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> END
 
