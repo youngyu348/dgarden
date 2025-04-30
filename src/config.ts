405afe33800b6429b1031877608422ab21b62c34
youngyu348 <youngyue2018@outlook.com>
2025-04-30 21:31:22 +0100

    修改邮箱地址

--- src/config.ts
+++ src/config.ts
@@ -138,7 +138,7 @@ export const themeConfig: ThemeConfig = {
     links: [
       {
         name: '来信',
-        url: 'https://youngyue2018@outlook.com', // rss.xml, atom.xml
+        url: 'mailto:youngyue2018@outlook.com', // rss.xml, atom.xml
       },
       // {
       //   name: 'Email',
