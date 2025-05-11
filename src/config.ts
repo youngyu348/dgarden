493b7168c8bef9b0e834d9c170b6668ec9ac5e03
youngyu348 <youngyue2018@outlook.com>
2025-05-11 20:09:43 +0100

    fix: update site subtitle for improved clarity

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const themeConfig: ThemeConfig = {
     // site title
     title: '犹昔人',
     // site subtitle
-    subtitle: '引譬连类',
+    subtitle: '引譬连类，返璞归真',
     // site description
     description: '',
     // use i18n title/subtitle/description from src/i18n/ui.ts instead of static ones above
